b6f761161f6bd642e07ad5784a604b07becf2928
TlangelaniBrian <mrbtmkhabela@gmail.com>
2023-10-26 22:22:14 +0200

    created-ini

---
+++ src/main/java/com/Alvu/flxmarkets/FlxmarketsApplication.java
@@ -0,0 +1,13 @@
+package com.Alvu.flxmarkets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlxmarketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlxmarketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Alvu/flxmarkets/FlxmarketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Alvu.flxmarkets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlxmarketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
